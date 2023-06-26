91d5ba1a8da8c45de8cdcf4ed23fdf84e47e56c0
First Last <you@email.com>
2023-06-26 13:38:44 +0800

    App endpoint

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var express = require( 'express' )
-var app = express()
-var port = process.env.PORT || 3000
-
-app.use( express.static('public') )
-
-app.listen( port, () => console.log( `App listening on port ${port}!`) )
+var express = require('express');  
+var app = express();  
+var port = process.env.PORT || 3000;  
+  
+app.use(express.static('public'));  
+app.use(express.json()); // Add this line to parse JSON request body  
+  
+// Add the following block to handle POST requests to /displayRequest  
+app.post('/displayRequest', (req, res) => {  
+  console.log('Received JSON request body:', req.body);  
+  res.json(req.body);  
+});  
+  
+app.listen(port, () => console.log(`App listening on port ${port}!`));  
